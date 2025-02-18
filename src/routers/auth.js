f8f790ec76e46b093cc8e27d0d2e69ffe0ce1087
an-na-kuzmina <anna.bychkova2008@gmail.com>
2025-02-19 00:21:15 +0200

    google auth and swagger contacts

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -11,6 +11,9 @@ import { sendResetEmailSchema } from '../validation/auth.js';
 import { sendResetEmailController } from '../controllers/auth.js';
 import { resetPasswordSchema } from '../validation/auth.js';
 import { resetPasswordController } from '../controllers/auth.js';
+import { getGoogleOAuthUrlController } from '../controllers/auth.js';
+import { loginWithGoogleOAuthSchema } from '../validation/auth.js';
+import { loginWithGoogleController } from '../controllers/auth.js';
 
 const router = Router();
 router.post('/refresh', ctrlWrapper(refreshUserSessionController));
@@ -37,6 +40,14 @@ router.post(
   '/reset-pwd',
   validateBody(resetPasswordSchema),
   ctrlWrapper(resetPasswordController),
+);
+
+router.get('/get-oauth-url', ctrlWrapper(getGoogleOAuthUrlController));
+
+router.post(
+  '/confirm-oauth',
+  validateBody(loginWithGoogleOAuthSchema),
+  ctrlWrapper(loginWithGoogleController),
 );
 
 export default router;
